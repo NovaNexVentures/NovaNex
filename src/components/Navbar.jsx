963043fdafda31ab0edebe5882abf127fdd5581f
Muhib <m.muhibk22@.com>
2025-09-28 17:47:49 +0500

    feat: initialize tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react"
+
+function Navbar(){
+    return (<nav className="fixed w-full z-40 top-0 left-0 bg-black" >
+    <h1>hello</h1>        
+    </nav>)
+}
+export default Navbar;
