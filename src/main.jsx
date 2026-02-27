56d8c02a27700ac4c9ac4536285feb51b60c2320
Sher Hassan <sherhassan739@gmail.com>
2026-02-28 00:32:23 +0500

    Remove Solana wallet integration from main.jsx
    Removed Solana wallet integration and related imports.

--- src/main.jsx
+++ src/main.jsx
@@ -4,40 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
-import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
-import { clusterApiUrl } from '@solana/web3.js'
-import '@solana/wallet-adapter-react-ui/styles.css'
 
-const endpoint = clusterApiUrl('mainnet-beta')
 
 function Root() {
-  // Use useMemo to ensure wallets are only initialized once
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-    ],
-    []
-  )
 
   return (
     <BrowserRouter>
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect={false} >
-          <WalletModalProvider>
             <App />
-          </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
     </BrowserRouter>
   );
 }
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <Root />
-  </StrictMode>,
 )
