3c6987007056894b27ff0cb29bddb92380370e81
Sher Hassan <sherhassan739@gmail.com>
2026-02-28 00:50:51 +0500

    syncsync

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@
 import { useState } from 'react'
 import Nav from './components/Nav'
 import Home from './pages/Home'
-import SwapModal from './components/SwapModal'
+
 
 export default function App() {
-  const [swapOpen, setSwapOpen] = useState(false)
-
   return (
     <>
-      <Nav onSwapClick={() => setSwapOpen(true)} />
-      <Home onSwapClick={() => setSwapOpen(true)} />
+      <Nav/>
+      <Home/>
 
-      {swapOpen && (
-        <SwapModal onClose={() => setSwapOpen(false)} />
-      )}
     </>
   )
 }
